41ac1cddd900b291326863f5263fe31e43466fae
neuberfran <neuberfran@gmail.com>
2020-12-19 21:53:58 -0300

    first commit

---
+++ link-account.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-link-account',
+  templateUrl: './link-account.component.html',
+  styleUrls: ['./link-account.component.css']
+})
+export class LinkAccountComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
